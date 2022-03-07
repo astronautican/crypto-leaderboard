728c6143234f1cdf0bd3202b3f06ea77332e6233
astronautican <dozataio@gmail.com>
2022-03-07 12:05:29 -0800

    updating text

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="flex justify-center">
     <div class="columns-3">
-      <div>colum</div>
-      <div>colum</div>
+      <div>colum1</div>
       <div>colum2</div>
+      <div>colum3</div>
     </div>
   </div>
 </template>
